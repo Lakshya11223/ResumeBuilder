e6f556ec1ade4cea7d28f0de0c5f8fb2f1215d48
Lakshya11223 <lakshyadeepsharma61@gmail.com>
2026-07-29 00:10:25 +0530

    updated server file

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ const app = express();
 app.use(express.json({limit:'1mb'}));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true,limit:'1mb'}))
-app.use(cors({
-   origin: "http://localhost:5173",  
-   methods: ["GET", "POST", "PUT", "DELETE"],
-   credentials: true
-}));
+// app.use(cors({
+//    origin: "http://localhost:5173",  
+//    methods: ["GET", "POST", "PUT", "DELETE"],
+//    credentials: true
+// }));
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 db();
