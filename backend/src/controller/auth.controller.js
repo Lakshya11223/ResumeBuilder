63821d2228cde43936b82590cb1b09d4b8a215ab
Lakshya11223 <lakshyadeepsharma61@gmail.com>
2026-07-29 17:53:34 +0530

    transporter updated

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -44,8 +44,11 @@ const registeruser = async (req, res) => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true only for port 465
+  port: 465,
+  secure: true,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
